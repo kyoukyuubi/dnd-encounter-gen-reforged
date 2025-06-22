c8a30b1767230b1f3b633452e70daf61c42c53cc
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-06-22 19:27:42 +0200

    modified: changed the type command for better formatting and error handling

--- command_type.go
+++ command_type.go
@@ -11,13 +11,26 @@ import (
 func commandType(cfg *Config, args ...string) error {
 	// if no args are provided display what is in the config
 	if len(args) == 0 {
-		fmt.Printf("Types are set to %v\n", cfg.Config.Types)
+		// check if the stored types are empty
+		if len(cfg.Config.Types) == 0 {
+			fmt.Println("Types are set to any")
+		} else {
+			fmt.Println("Types are set to: ")
+
+			// loop through the stored types and print them in an unordered list
+			for _, t := range cfg.Config.Types {
+				fmt.Printf("* %s\n", t)
+			}
+		}
+
+		fmt.Println("")
 		return nil
 	}
 
 	// if args is set to 'Any' reset the config to an empty slice
 	if args[0] == "any" {
 		fmt.Println("types has been reset to default value")
+		fmt.Println("")
 		cfg.Config.Types = []string{}
 		return nil
 	}
@@ -27,29 +40,56 @@ func commandType(cfg *Config, args ...string) error {
 	if err != nil {
 		errorhandling.LogError(err, "commandType")
 		fmt.Println("Error occurred, check the log in json/logs for details")
+		fmt.Println("")
 		return nil
 	}
 
 	// create an empty slice for putting the found values into
 	var sliceTypes []string
 
-	// loop through the types gotten from the json and check if inputted text matches
-	for _, t := range types {
-		for _, input := range args {
+	// set a value to false, to be used after the loop to see if formatting needs to be printed
+	errorOccurred := false
+
+	// loop through the user inputs and see if they match the types from the json
+	for _, input := range args {
+		matchFound := false
+		for _, t := range types {
 			// check if what was inputtet matches what was in the json file
 			// uses the value from the json file for error handling etc.
 			if strings.Contains(strings.ToLower(t), input) {
-				sliceTypes= append(sliceTypes, t)
+				sliceTypes = append(sliceTypes, t)
+				matchFound = true
 				continue
 			}
 		}
+
+		// if no matches is found tell the user
+		if !matchFound {
+			fmt.Printf("%s was not found in the determined types\n", input)
+			errorOccurred = true
+		}
+	}
+	
+	// check if error occurred, if so print a line break for the user to make things easier to read
+	if errorOccurred {
+		fmt.Println("-------------------------------")
 	}
 
-	// show the user what the types are now set to
-	fmt.Printf("The types are now set to %v\n", sliceTypes)
+	// update the config with the new slice if the new slice list is not empty
+	if len(sliceTypes) != 0 {
+		cfg.Config.Types = sliceTypes
+		fmt.Println("The types are now set to:")
 
-	// update the config with the new slice
-	cfg.Config.Types = sliceTypes
+		// loop through the created slice and print it in an unorederd list format
+		for _, t := range sliceTypes {
+			fmt.Printf("* %s\n", t)
+		}
+		fmt.Println("")
+	} else {
+		fmt.Println("No matches were found in the determined types")
+		fmt.Println("types list was not altered")
+		fmt.Println("")
+	}
 
 	return nil
 }
